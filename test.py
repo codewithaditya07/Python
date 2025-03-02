3e01667b6560d634d897edce11659c5021110586
codewithaditya07 <adityasahani680@gmail.com>
2025-03-02 22:46:41 +0530

    Python

--- test.py
+++ test.py
@@ -29,10 +29,16 @@ print(second_largest([10, 20, 4, 45, 99]))  # 45
 
 
 # 6. Count occurrences of each character in a string
-from collections import Counter
-def char_count(s):
-    return dict(Counter(s))
-print(char_count("hello"))  # {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+def count_characters(s):
+    char_count = {}
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+    return char_count
+
+# Example usage
+string = "hello world"
+result = count_characters(string)
+print(result)
 
 
 # 7. Reverse a list without using reverse() method
