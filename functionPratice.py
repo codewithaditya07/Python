c82e9e1630a83e8971866e16dd2e9da82c825ac6
codewithaditya07 <adityasahani680@gmail.com>
2025-03-16 22:45:38 +0530

    Python

--- functionPratice.py
+++ functionPratice.py
@@ -14,10 +14,21 @@
 # Write a program to create function calculation() such that it can accept two variables and calculate addition and subtraction. 
 # Also, it must return both addition and subtraction in a single return call.
 
-def  calculation(a,b):
-    add = a+b
-    sub = a-b
-    return add,sub
+# def  calculation(a,b):
+#     add = a+b
+#     sub = a-b
+#     return add,sub
 
-cal = calculation(80,40)
-print(cal)
+# cal = calculation(80,40)
+# print(cal)
+
+
+# Write a program to create a function show_employee() using the following conditions.
+# It should accept the employee’s name and salary and display both.
+# If the salary is missing in the function call then assign default value 9000 to salary
+
+def show_employee(name, salary=9000):
+    print("Name:", name, "salary:", salary)
+
+show_employee("Ben", 12000)
+show_employee("Jessa")
