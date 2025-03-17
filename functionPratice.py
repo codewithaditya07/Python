8a7db888af0bb80e9eb3e9b35ec7e307661d77e0
codewithaditya07 <adityasahani680@gmail.com>
2025-03-17 23:26:50 +0530

    Python

--- functionPratice.py
+++ functionPratice.py
@@ -27,8 +27,28 @@
 # It should accept the employee’s name and salary and display both.
 # If the salary is missing in the function call then assign default value 9000 to salary
 
-def show_employee(name, salary=9000):
-    print("Name:", name, "salary:", salary)
+# def show_employee(name, salary=9000):
+#     print("Name:", name, "salary:", salary)
 
-show_employee("Ben", 12000)
-show_employee("Jessa")
+# show_employee("Ben", 12000)
+# show_employee("Jessa")
+
+# Create an inner function to calculate the addition in the following way
+# Create an outer function that will accept two parameters, a and b
+# Create an inner function inside an outer function that will calculate the addition of a and b
+# At last, an outer function will add 5 into addition and return it
+# # outer function
+def outer_fun(a, b):
+    square = a ** 2
+
+    # inner function
+    def addition(a, b):
+        return a + b
+
+    # call inner function from outer function
+    add = addition(a, b)
+    # add 5 to the result
+    return add + 5
+
+result = outer_fun(5, 10)
+print(result)
