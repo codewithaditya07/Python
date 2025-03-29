e358f4f1e77220eb716496634d74e3c4438559ec
codewithaditya07 <adityasahani680@gmail.com>
2025-03-29 21:38:38 +0530

    Python

--- StringPracice.py
+++ StringPracice.py
@@ -93,4 +93,6 @@ def find_digits_chars_symbols(sample_str):
 
     print("Chars =", char_count, "Digits =", digit_count, "Symbol =", symbol_count)
 
-
+sample_str = "P@yn2at&#i5ve"
+print("total counts of chars, Digits, and symbols \n")
+find_digits_chars_symbols(sample_str)
