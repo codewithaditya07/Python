4a0ea0acf6ec63adcab807fcb0dd355ab9203dea
codewithaditya07 <adityasahani680@gmail.com>
2025-04-01 22:07:19 +0530

    Python

--- StringPracice.py
+++ StringPracice.py
@@ -78,20 +78,43 @@ print("New String:", res)
 
 #  Count all letters, digits, and special symbols from a given string
 
-def find_digits_chars_symbols(sample_str):
-    char_count = 0
-    digit_count = 0
-    symbol_count = 0
-    for char in sample_str:
-        if char.isalpha():
-            char_count += 1
-        elif char.isdigit():
-            digit_count += 1       
+# def find_digits_chars_symbols(sample_str):
+#     char_count = 0
+#     digit_count = 0
+#     symbol_count = 0
+#     for char in sample_str:
+#         if char.isalpha():
+#             char_count += 1
+#         elif char.isdigit():
+#             digit_count += 1       
+#         else:
+#             symbol_count += 1
+
+#     print("Chars =", char_count, "Digits =", digit_count, "Symbol =", symbol_count)
+
+# sample_str = "P@yn2at&#i5ve"
+# print("total counts of chars, Digits, and symbols \n")
+# find_digits_chars_symbols(sample_str)
+
+# Write a program to check if two strings are balanced. For example, 
+# strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter.
+
+def string_balance_test(s1, s2):
+    flag = True
+    for char in s1:
+        if char in s2:
+            continue
         else:
-            symbol_count += 1
+            flag = False
+    return flag
 
-    print("Chars =", char_count, "Digits =", digit_count, "Symbol =", symbol_count)
 
-sample_str = "P@yn2at&#i5ve"
-print("total counts of chars, Digits, and symbols \n")
-find_digits_chars_symbols(sample_str)
+s1 = "Yn"
+s2 = "PYnative"
+flag = string_balance_test(s1, s2)
+print("s1 and s2 are balanced:", flag)
+
+s1 = "Ynf"
+s2 = "PYnative"
+flag = string_balance_test(s1, s2)
+print("s1 and s2 are balanced:", flag)
