74473804ecadee9e5007fa9d1856a088a1135348
codewithaditya07 <adityasahani680@gmail.com>
2025-04-11 23:14:37 +0530

    Python

--- StringPracice.py
+++ StringPracice.py
@@ -164,21 +164,17 @@
 
 # Reverse a given string
 
-str1 = "PYnative"
-print("Original String is:", str1)
+# str1 = "PYnative"
+# print("Original String is:", str1)
 
-str1 = str1[::-1]
-print("Reversed String is:", str1)
+# str1 = str1[::-1]
+# print("Reversed String is:", str1)
 
 
 # Write a program to count occurrences of all characters within a string
 str1 = "Apple"
-
-# create a result dictionary
 char_dict = dict()
-
 for char in str1:
     count = str1.count(char)
-    # add / update the count of a character
     char_dict[char] = count
 print('Result:', char_dict)
