9bbc9545f54b021b7aea0ca00153c4801cbdce9a
codewithaditya07 <adityasahani680@gmail.com>
2025-04-19 16:21:54 +0530

    Python

--- Loops.py
+++ Loops.py
@@ -15,3 +15,20 @@ for i in range(1,10,2):
 for i in range(10,1,-1):
     print(i)
 
+# 
+for i in range(10,1,-2):
+    print(i)
+
+# strings
+str="Krish Naik"
+
+for i in str:
+    print(i)
+
+# Nested loopss
+## a loop inside a loop
+
+for i in range(3):
+    for j in range(2):
+        print(f"i:{i} and j:{j}")
+
