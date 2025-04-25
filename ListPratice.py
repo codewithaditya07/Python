84c386774140dff46e17215c4dbbd0bc3fdb7b3d
codewithaditya07 <adityasahani680@gmail.com>
2025-04-25 23:31:02 +0530

    Python

--- ListPratice.py
+++ ListPratice.py
@@ -5,7 +5,17 @@
 # print(list1)
 
 #Q2 Concatenate two lists index-wise
-list1 = ["M", "na", "i", "Ke"] 
-list2 = ["y", "me", "s", "lly"]
-list3 = [i + j for i, j in zip(list1, list2)]
-print(list3)
+
+# list1 = ["M", "na", "i", "Ke"] 
+# list2 = ["y", "me", "s", "lly"]
+# list3 = [i + j for i, j in zip(list1, list2)]
+# print(list3)
+
+# Q3 Turn every item of a list into its square
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+res = []
+for i in numbers:
+    res.append(i * i)
+print(res)
+
