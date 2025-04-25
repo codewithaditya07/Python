f6e6c9c880549b60eba82254d3e3e78c6a64ff70
codewithaditya07 <adityasahani680@gmail.com>
2025-04-25 23:24:07 +0530

    Python

--- ListPratice.py
+++ ListPratice.py
@@ -1,4 +1,11 @@
-# Reverse a list in Python
-list1 = [100, 200, 300, 400, 500]
-list1.reverse()
-print(list1)
+# Q1 Reverse a list in Python
+
+# list1 = [100, 200, 300, 400, 500]
+# list1.reverse()
+# print(list1)
+
+#Q2 Concatenate two lists index-wise
+list1 = ["M", "na", "i", "Ke"] 
+list2 = ["y", "me", "s", "lly"]
+list3 = [i + j for i, j in zip(list1, list2)]
+print(list3)
