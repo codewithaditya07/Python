5605ce51a34afcb73d2cde3abe0b80647009f000
codewithaditya07 <adityasahani680@gmail.com>
2025-04-28 23:21:29 +0530

    Python

--- DictionaryPratice.py
+++ DictionaryPratice.py
@@ -25,3 +25,13 @@ sampleDict = {
     }
 }
 print(sampleDict['class']['student']['marks']['history'])
+
+#Q4 Initialize dictionary with default values
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+res = dict.fromkeys(employees, defaults)
+print(res)
+
+# Individual data
+print(res["Kelly"])
