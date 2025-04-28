6c1efab6c83e8432063aed8e6029802877ec330f
codewithaditya07 <adityasahani680@gmail.com>
2025-04-28 23:15:21 +0530

    Python

--- ListPratice.py
+++ ListPratice.py
@@ -55,3 +55,11 @@ list1 = [5, 10, 15, 20, 25, 50, 20]
 index = list1.index(20)
 list1[index] = 200
 print(list1)
+
+# Q10 Remove all occurrences of a specific item from a list.
+list1 = [5, 20, 15, 20, 25, 50, 20]
+def remove_value(sample_list, val):
+    return [i for i in sample_list if i != val]
+
+res = remove_value(list1, 20)
+print(res)
