9673a3668ff5cb916288fc082b0f722ad0efb7d1
codewithaditya07 <adityasahani680@gmail.com>
2025-04-30 21:37:18 +0530

    Python

---
+++ SetPratice.py
@@ -0,0 +1,19 @@
+#Q1 Add a list of elements to a set
+sample_set = {"Yellow", "Orange", "Black"}
+sample_list = ["Blue", "Green", "Red"]
+
+sample_set.update(sample_list)
+print(sample_set)
+
+#Q2 Return a new set of identical items from two sets
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+print(set1.intersection(set2))
+#Q3
+#Q4
+#Q5
+#Q6
+#Q7
+#Q8
+#Q9
