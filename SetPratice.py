14d08614056a0c91eb88cb79c4d2af7bdb6cd3e4
codewithaditya07 <adityasahani680@gmail.com>
2025-05-02 23:37:12 +0530

    Python

--- SetPratice.py
+++ SetPratice.py
@@ -10,8 +10,20 @@ set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 
 print(set1.intersection(set2))
-#Q3
-#Q4
+
+#Q3 Get Only unique items from two sets
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+print(set1.union(set2))
+
+#Q4 Update the first set with items that don’t exist in the second set
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+
+set1.difference_update(set2)
+print(set1)
+
 #Q5
 #Q6
 #Q7
