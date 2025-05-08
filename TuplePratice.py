c24ef0c3f571d75ed9740e2df6a4ceaaab4288f3
codewithaditya07 <adityasahani680@gmail.com>
2025-05-08 23:14:28 +0530

    Python

--- TuplePratice.py
+++ TuplePratice.py
@@ -3,4 +3,10 @@ tuple1 = (10, 20, 30, 40, 50)
 tuple1 = tuple1[::-1]
 print(tuple1)
 
+# Q2 Access value 20 from the tuple
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+print(tuple1[1][1])
 
+# Q3 Create a tuple with single item 50
+tuple1= (50, )
+print(tuple1)
