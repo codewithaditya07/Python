956573e50a53300138525530e4dbaca4728aba33
codewithaditya07 <adityasahani680@gmail.com>
2025-05-09 17:37:50 +0530

    Python

--- TuplePratice.py
+++ TuplePratice.py
@@ -10,3 +10,12 @@ print(tuple1[1][1])
 # Q3 Create a tuple with single item 50
 tuple1= (50, )
 print(tuple1)
+
+# Q4 Unpack the tuple into 4 variables
+tuple1 = (10, 20, 30, 40)
+
+a, b, c, d = tuple1
+print(a)
+print(b)
+print(c)
+print(d)
