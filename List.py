c48994fa01c82631ad84ef97715a4ba127cf531d
codewithaditya07 <adityasahani680@gmail.com>
2025-09-07 22:19:28 +0530

    C

--- List.py
+++ List.py
@@ -6,10 +6,10 @@
 #         break
 #     else:
 #         pass # print("multiple of 10 :" , i)
-list = [10,20,30,3,40,50]
-for i in list:
-  while(i%10 != 0):
-   print("Not a multiple of 10 is " , i)
-   break
+# list = [10,20,30,3,40,50]
+# for i in list:
+#   while(i%10 != 0):
+#    print("Not a multiple of 10 is " , i)
+#    break
   
  
