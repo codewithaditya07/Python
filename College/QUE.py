fb8e0561c2a5ba924946f9e9af2ecbaac7298406
codewithaditya07 <adityasahani680@gmail.com>
2025-09-13 15:13:41 +0530

    C

--- College/QUE.py
+++ College/QUE.py
@@ -14,17 +14,17 @@
 #         print("Bot: I'm not sure how to respond to that.")
 
 # Q2.To implement a reflex agent in Python that decides whether to carry an umbrella or a water bottle.
-# print("Menu:")
-# print("1. Exit")
-# print("2. Weather decision")
-# choice = input("Enter your choice (1 or 2): ")
-# if choice == "1":
-#     print("Exiting...")
-# elif choice == "2":
-#     weather = input("Enter today's weather: ").lower()
-#     if "rain" in weather:
-#         print("Agent: Take an umbrella")
-#     else:
-#         print("Agent: Carry a water bottle")
-# else:
-#     print("Invalid choice!")
+print("Menu:")
+print("1. Exit")
+print("2. Weather decision")
+choice = input("Enter your choice (1 or 2): ")
+if choice == "1":
+    print("Exiting...")
+elif choice == "2":
+    weather = input("Enter today's weather: ").lower()
+    if "rain" in weather:
+        print("Agent: Take an umbrella")
+    else:
+        print("Agent: Carry a water bottle")
+else:
+    print("Invalid choice!")
