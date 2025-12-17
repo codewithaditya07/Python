cc334ea1cd14d2645a5b02087f4cfb0b95409b3a
codewithaditya07 <adityasahani680@gmail.com>
2025-12-17 22:24:25 +0530

    C

--- com.py
+++ com.py
@@ -75,18 +75,18 @@
 #    print("invalid operation" )
 
 
-# i = 1
-# while i <= 5:
-#     print(i)
-#     i = i + 1
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
 
 
-# i = 1
-# while i <= 5:
-#     print(i * "*")
-#     i = i + 1
+i = 1
+while i <= 5:
+    print(i * "*")
+    i = i + 1
 
-# i = 5
-# while i >= 0:
-#     print(i * "*")
-#     i = i - 1
+i = 5
+while i >= 0:
+    print(i * "*")
+    i = i - 1
