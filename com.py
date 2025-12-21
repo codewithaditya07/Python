bee8d4eb4ce92a18d50bc06a10f5381ea0f689e0
codewithaditya07 <adityasahani680@gmail.com>
2025-12-21 20:49:29 +0530

    C

--- com.py
+++ com.py
@@ -81,12 +81,12 @@ while i <= 5:
     i = i + 1
 
 
-i = 1
-while i <= 5:
-    print(i * "*")
-    i = i + 1
+# i = 1
+# while i <= 5:
+#     print(i * "*")
+#     i = i + 1
 
-i = 5
-while i >= 0:
-    print(i * "*")
-    i = i - 1
+# i = 5
+# while i >= 0:
+#     print(i * "*")
+#     i = i - 1
