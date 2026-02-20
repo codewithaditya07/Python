05eb070424af950c5782d2b6b7459b2f46cc2701
codewithaditya07 <adityasahani680@gmail.com>
2026-02-20 21:16:57 +0530

    C

--- College/LABSU.py
+++ College/LABSU.py
@@ -113,14 +113,14 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 # Arithmetic Operators
-print("\nArithmetic Operators")
-print("Addition =", a + b)
-print("Subtraction =", a - b)
-print("Multiplication =", a * b)
-print("Division =", a / b)
-print("Modulus =", a % b)
-print("Exponent =", a ** b)
-print("Floor Division =", a // b)
+# print("\nArithmetic Operators")
+# print("Addition =", a + b)
+# print("Subtraction =", a - b)
+# print("Multiplication =", a * b)
+# print("Division =", a / b)
+# print("Modulus =", a % b)
+# print("Exponent =", a ** b)
+# print("Floor Division =", a // b)
 
 # Relational (Comparison) Operators
 print("\nRelational Operators")
