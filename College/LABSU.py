3becd29262e90af46f52d65132b3499336daf956
codewithaditya07 <adityasahani680@gmail.com>
2026-02-21 23:54:20 +0530

    C

--- College/LABSU.py
+++ College/LABSU.py
@@ -109,8 +109,8 @@
 # 7	WAP to implement various operators	
 # Program to demonstrate various operators
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
 
 # Arithmetic Operators
 # print("\nArithmetic Operators")
@@ -123,33 +123,46 @@ b = int(input("Enter second number: "))
 # print("Floor Division =", a // b)
 
 # Relational (Comparison) Operators
-print("\nRelational Operators")
-print("a > b :", a > b)
-print("a < b :", a < b)
-print("a == b :", a == b)
-print("a != b :", a != b)
-print("a >= b :", a >= b)
-print("a <= b :", a <= b)
+# print("\nRelational Operators")
+# print("a > b :", a > b)
+# print("a < b :", a < b)
+# print("a == b :", a == b)
+# print("a != b :", a != b)
+# print("a >= b :", a >= b)
+# print("a <= b :", a <= b)
 
-# Logical Operators
-print("\nLogical Operators")
-print("(a > 0 and b > 0) :", a > 0 and b > 0)
-print("(a > 0 or b > 0) :", a > 0 or b > 0)
-print("not(a > b) :", not(a > b))
+# # Logical Operators
+# print("\nLogical Operators")
+# print("(a > 0 and b > 0) :", a > 0 and b > 0)
+# print("(a > 0 or b > 0) :", a > 0 or b > 0)
+# print("not(a > b) :", not(a > b))
 
-# Assignment Operators
-print("\nAssignment Operators")
-c = a
-c += b
-print("c += b :", c)
-c -= b
-print("c -= b :", c)
+# # Assignment Operators
+# print("\nAssignment Operators")
+# c = a
+# c += b
+# print("c += b :", c)
+# c -= b
+# print("c -= b :", c)
 
-# Bitwise Operators
-print("\nBitwise Operators")
-print("a & b =", a & b)
-print("a | b =", a | b)
-print("a ^ b =", a ^ b)
-print("~a =", ~a)
-print("a << 1 =", a << 1)
-print("a >> 1 =", a >> 1)
+# # Bitwise Operators
+# print("\nBitwise Operators")
+# print("a & b =", a & b)
+# print("a | b =", a | b)
+# print("a ^ b =", a ^ b)
+# print("~a =", ~a)
+# print("a << 1 =", a << 1)
+# print("a >> 1 =", a >> 1)
+
+
+# x = [1,2,3]
+# print(x*2)
+
+# for i in range(1,5):
+#     if i==3:
+#         break
+    # print(i,end=" ")
+    
+
+
+# print(bool(0), bool(5))
