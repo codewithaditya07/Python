9aec502e5eedaec7b0fe257020cd3ce5afe1e8bd
codewithaditya07 <adityasahani680@gmail.com>
2026-04-02 23:52:21 +0530

    C

--- College/LABSU.py
+++ College/LABSU.py
@@ -20,10 +20,10 @@
 # import math
 
 # # Taking input from user
-# x1 = float(input("Enter x1: "))
-# y1 = float(input("Enter y1: "))
-# x2 = float(input("Enter x2: "))
-# y2 = float(input("Enter y2: "))
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
 
 # # Applying distance formula
 # distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
