423aa5629063576b3b02813abd52e17c22877f2e
codewithaditya07 <adityasahani680@gmail.com>
2026-04-23 23:44:24 +0530

    C

---
+++ College/examm.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data={
+    "name":["aditya","advaya","pari"],
+    "age":[21,26,20],
+    "marks":[85,61,94],
+    "city":["delhi","up","gr noida"]
+
+}
+
+db = pd.DataFrame(data)
+print(db)
