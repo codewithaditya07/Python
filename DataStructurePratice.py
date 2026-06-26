db8c9c762241209d02107b59ec2064ca4dc3381a
codewithaditya07 <adityasahani680@gmail.com>
2026-06-26 23:30:48 +0530

    py

--- DataStructurePratice.py
+++ DataStructurePratice.py
@@ -21,12 +21,12 @@ print(res)
 # /
 sample_list = [34, 54, 67, 89, 11, 43, 94]
 
-print("Original list ", sample_list)
-# element = sample_list.pop(4)
-# print("List After removing element at index 4 ", sample_list)
+# print("Original list ", sample_list)
+# # element = sample_list.pop(4)
+# # print("List After removing element at index 4 ", sample_list)
 
-# sample_list.insert(2, element)
-# print("List after Adding element at index 2 ", sample_list)
+# # sample_list.insert(2, element)
+# # print("List after Adding element at index 2 ", sample_list)
 
-# sample_list.append(element)
-# print("List after Adding element at last ", sample_list)
+# # sample_list.append(element)
+# # print("List after Adding element at last ", sample_list)
