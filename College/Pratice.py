96e27ac87c6403e66fa270b1dd7b973094b0e8ad
codewithaditya07 <adityasahani680@gmail.com>
2026-09-03 00:16:14 +0530

    py

---
+++ College/Pratice.py
@@ -0,0 +1,22 @@
+# Q1 Write a Python program using Pandas to detect and fill missing values with the mean.
+# import pandas as pd
+
+# df = pd.DataFrame({
+#     'age': [20,30,None,60],
+#     'marks': [ 80,None,40,90]
+# })
+
+# df.fillna(df.mean(numeric_only=True), inplace=True)
+# print(df)
+
+# Q2 Write Python code to perform One-Hot Encoding.
+ 
+import pandas as pd
+hash
+data = pd.DataFrame({
+ 'color': ['red','blue','green']
+ 
+})
+license
+df = pd.get_dummies(data)
+print(df)
